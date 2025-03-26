92137f36b7de9e54ef550d7b2b775b159d0e18ca
Fuganti <juliocesarfuganti@gmail.com>
2025-03-26 20:18:28 -0300

    testing

--- project/DataStorage.java
+++ project/DataStorage.java
@@ -2,6 +2,10 @@ import Manager.DataManager;
 
 public class DataStorage {
     public static void main(String[] args) {
+        DataManager dm = new DataManager();
+        dm.add_student("Rogerio", "rogerio@usp.br", "icmc", 1, 5);
+        dm.add_professor("Cleber", "cleber@usp.br", "icmc", 0, true);
 
+        dm.list_professors_per_institute("icmc");
     }
 }
--- project/Manager/DataManager.java
+++ project/Manager/DataManager.java
@@ -3,11 +3,17 @@ package Manager;
 import entities.Student;
 import entities.Professor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataManager {
     private List<Student> students;
     private List<Professor> professors;
+
+    public DataManager() {
+        this.students = new ArrayList<Student>();
+        this.professors = new ArrayList<>();
+    }
 
     public void add_student(String Name, String Email, String Institute, int Number, double Grade) {
         Student st = new Student(Name, Email, Institute, Number, Grade);
@@ -21,31 +27,43 @@ public class DataManager {
         System.out.println("Professor added\n");
     }
 
-    public void get_student_by_number(int number) {
+    private Student get_student_by_number(int number) {
         for (Student st : students) {
             if (st.get_number() == number) {
-                System.out.println("Name: " + st.get_name() + "\n Email: " + st.get_email() + "\n Institute: " + st.get_institute());
-                if(st.isapproved()) {
-                    System.out.println("\n Approved\n");
-                    break;
-                }
-                System.out.println("Not Approved\n");
-                break;
+                return st;
             }
+            System.out.println("Student not found in the database\n");
         }
+        return null;
     }
 
-    public void get_professor_by_number(int number) {
+    private Professor get_professor_by_number(int number) {
         for (Professor pf : professors) {
             if (pf.get_number() == number) {
-                System.out.println("Name: " + pf.get_name() + "\n Email: " + pf.get_email() + "\n Institute: " + pf.get_institute());
-                if(pf.isprofessorON()) {
-                    System.out.println("\nThis professor is Working nowadays\n");
-                    break;
-                }
-                System.out.println("This professor is not currently Working at this institute\n");
-                break;
+                return pf;
             }
+            System.out.println("professor not found in the data base\n");
+        }
+        return null;
+    }
+
+    public void list_professors_per_institute(String institute) {
+        if (professors.isEmpty()) {
+            System.out.println("No professors found\n");
+            return;
+        }
+
+        int flag = 0;
+
+        System.out.println("Professors that works in " + institute + ":\n");
+        for (Professor pf : professors) {
+            if (pf.get_institute().equals(institute)) {
+                flag = 1;
+                System.out.println(pf.get_name() + "\nemail for contact: " + pf.get_email() + "\n");
+            }
+        }
+        if (flag == 0) {
+            System.out.println("No professors found\n");
         }
     }
 }
